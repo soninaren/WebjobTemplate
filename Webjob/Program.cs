8bbb118d1bad8cf52e67231d0a6fb12b006fe1c2
Naren Soni <nasoni@microsoft.com>
2022-06-07 10:56:18 -0700

    Add project files.

---
+++ Webjob/Program.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Threading.Tasks;
+
+namespace WebJob
+{
+    internal class Program
+    {
+        public static async Task Main()
+        {
+
+            var host = new HostBuilder()
+                .UseEnvironment("Development")
+                .ConfigureLogging((context, b) =>
+                {
+                    b.SetMinimumLevel(LogLevel.Debug);
+                    b.AddConsole();
+                }).Build();
+
+            await host.RunAsync();
+        }
+    }
+}
